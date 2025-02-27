ec44ea5257420e22b89ee0a1a8e64eedfd21cdf3
Marie Caron <marie.caron@epitech.eu>
2025-02-27 18:55:50 +0100

    fix: body of the code.

--- ex_02.js
+++ ex_02.js
@@ -1,12 +1,12 @@
 function findHousing(housingToFind, housingList)
 {
-		let i = 0
+	let i = 0
 
-		while (i < housingList.length) {
-  			if (housingList[i] === housingToFind) {
+	while (i < housingList.length) {
+  		if (housingList[i] === housingToFind) {
         	return [i + 1, housingList[i]]
-        }
-     		i++
-   	 }
-     return null
+		}
+     	i++
+   	}
+    return null
 }
